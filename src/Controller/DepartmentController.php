cfa5d2604a892748fa2a41da86814cf62c11e51e
NomenjanaharyEric <rakotomaheryeric11@gmail.com>
2023-01-21 03:30:07 +0300

    Paginate Departments

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -6,13 +6,27 @@ use App\Repository\DepartmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class DepartmentController extends AbstractController
 {
-    #[Route('/department', name: 'app_department')]
-    public function index(DepartmentRepository $departmentRepository): Response
+    /**
+     * This method allow us to get department list function
+     *
+     * @param DepartmentRepository $departmentRepository
+     * @param PaginatorInterface $paginatorInterface
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/department', name: 'app_department', methods:["GET"])]
+    public function index(DepartmentRepository $departmentRepository, PaginatorInterface $paginatorInterface, Request $request): Response
     {
-        $departments = $departmentRepository->findAll();
+        $departments = $paginatorInterface->paginate(
+            $departmentRepository->findAll(),
+            $request->query->getInt("page", 1),
+            5
+        );
         return $this->render('pages/department/index.html.twig', [
             "departments" => $departments
         ]);
